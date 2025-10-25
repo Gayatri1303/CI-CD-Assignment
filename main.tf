2b94bb8d80be903b97bd8f245605a21c4416fa52
Gayatri1303 <gayatrikurulkar@gmail.com>
2025-10-25 15:24:13 +0000

    modified output.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "s3_bucket" {
+module "s3_buck" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = var.bucket
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "bucket" {
-    value = module.s3_bucket.bucket
+    value = module.s3_buck.s3_bucket_bucket_domain_name
 }
